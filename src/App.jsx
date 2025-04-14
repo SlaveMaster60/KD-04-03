0a4944b5f4e9af1f7d7f55909aa255addfba3ce9
Moksleivis <Moksleivis@kitm.local>
2025-04-14 14:20:43 +0300

    Tuscias

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,2 @@
 import { useEffect, useState } from 'react'
-
 import './App.css'
-import Navbar from './assets/Navbar'
-import tasks from './assets/tasks'
-import NewTask from './assets/NewTask'
-
-function App() {
-  const [tasks, setTasks] = useState({});
-    JSON.parse(localStorage.getItem('tasks'))
-
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
-
-  return (
-    <>
-    <Navbar />
-    <NewTask setTasks={setTasks} />
-    <Tasks tasks={tasks} /> 
-
-      </>
-  )
-}
-
-export default App
